3ce3e1c1c0977067b3d46b46bbb9556ce56083e9
Irene García <irenegwodak@gmail.com>
2025-02-19 12:24:12 +0100

    add intro js

---
+++ 03JS/01-intro/script.js
@@ -0,0 +1,15 @@
+// ENTRADA DE DATOS
+num1 = 10;
+num2 = 20;
+
+// PROCESO
+average = (num1 + num2) / 2;
+
+// SALIDA DE DATOS
+console.log('El primer número introducido es ' + num1);
+console.log('El segundo número introducido es ' + num2);
+console.log("EL proceso es: 'media  = (número 1 + número 2) /2'");
+
+console.log('La media es, ' + average);
+
+let num1 = parseInt(prompt('Numero 1: '));

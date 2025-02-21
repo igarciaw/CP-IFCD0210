d5688ffad1b91edfa1737b82e2d766290f0d5a14
Irene García <irenegwodak@gmail.com>
2025-02-21 11:15:11 +0100

    solve 08 area

---
+++ 03JS/Ejercicios/COMPLEMENTARIOS/08-area/script.js
@@ -0,0 +1,10 @@
+// 08-area
+// Diseña un algoritmo que calcule un área cuadrada. Un área cuadrada es su longitud * longitud.
+
+alert('Vamos a calcular el área de un cuadrado');
+let longitud = parseInt(prompt('Introduce la longitud en cm: '));
+let area = longitud * longitud;
+alert('El área del cuadrado es: ' + area + ' cm');
+console.log("Longitud ntroducida: " + longitud + " cm");
+
+console.log('El área del cuadrado es: ' + area + ' cm');

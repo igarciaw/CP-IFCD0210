0da003f534c602cc05ff5a853b4901b78b313c59
Irene García <irenegwodak@gmail.com>
2025-02-21 14:09:00 +0100

    solve 13 convertir pulgadas cm

---
+++ 03JS/Ejercicios/COMPLEMENTARIOS/13-convierte-pulgadas-a-centimetros/script.js
@@ -0,0 +1,8 @@
+// 13-convierte-pulgadas-a-centimetros
+// Escriba un algoritmo que convierta las medidas de pulgadas a centímetros, teniendo en cuenta que una pulgada son 2,54cm.
+
+let pulgadas = parseInt(prompt('Introduce las pulgadas: '));
+let centimetros = pulgadas * 2.54;
+
+alert(pulgadas + ' pulgadas en centímetros son: '+ centimetros + 'cm.');
+console.log(pulgadas + ' pulgadas en centímetros son: '+ centimetros + 'cm.');

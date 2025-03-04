c49317847649a5183cdb09c3e04d4bff544bf03c
Irene García <irenegwodak@gmail.com>
2025-03-04 14:19:27 +0100

    Add 22: numero de likes

---
+++ 03JS/Ejercicios/ENTREGABLES/22-sustitutivo-numero-de-likes/script.js
@@ -0,0 +1,8 @@
+// [SUSTITUTIVO] Número de likes
+// Escribe un programa en javascript que dado el número de likes, nos debe retornar el número de likes convertido a kilobytes o millones. Un kilobytes son 1000 bytes y 1 millón son 1000.000 bytes. 
+
+// Ejemplo: Si nos dan 1400 Likes se debería devolver 1 K (1,4)
+
+// Si nos dan 34567 se debería devolver 34K
+
+// Si nos dan 7456345 nos debería devolver 7 M

d27c3b4a19b94e42b84026b92f6ca60410536945
Irene García <irenegwodak@gmail.com>
2025-03-04 14:20:17 +0100

    Add 26: 21 blackjack

---
+++ 03JS/Ejercicios/ENTREGABLES/26-extra-blackjack-o-21/script.js
@@ -0,0 +1,9 @@
+// [EXTRA] BlackJack o 21
+// Escribe un programa en Javascript que realice el juego del BlackJack o 21.
+// El programa debe solicitar a un usuario si quiere jugar al juego
+// Si desea barajar las cartas de una baraja de 40 cartas
+// (se pueden usar librerías externas como shufftle de underscore si se desea)
+// Y escoger la mano de si piensa que plantarse, tirar jugada y compararla con la máquina y ver quien ha ganado o salir.
+// Para simplificar el juego vamos a tratar con 40 cartas y el que sume 21 gana, en caso de empatar gana el jugador. 
+// No contaremos con las cartas especiales.
+// El juego consiste en preguntar al usuario o a la máquina si se quiere plantar o continuar y el primero que llegue a 21 gana la partida. 

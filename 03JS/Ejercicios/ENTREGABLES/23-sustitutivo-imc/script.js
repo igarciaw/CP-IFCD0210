e15170a019a4513fcfb8aa1ce9565ade5bee4a9e
Irene García <irenegwodak@gmail.com>
2025-03-04 14:19:57 +0100

    Add 23: imc

---
+++ 03JS/Ejercicios/ENTREGABLES/23-sustitutivo-imc/script.js
@@ -0,0 +1,17 @@
+// [SUSTITUTIVO] IMC
+// Escribe un programa en Javascript que calcule el índice de masa corporal (IMC o BMI)
+
+// El IMC se calcula con la siguiente fórmula peso / altura ^2 ( altura elevado a 2 lo que es altura * altura)
+// Una vez aclarado su IMC escribir una una función que reciba 2 argumentos: peso y altura y retorne un string con las siguientes posibilidades:
+
+// "Bajo de peso" si el IIMC es menor de 18.5
+// "Normal" si el IMC está entre 18.5 y 24.9
+// "Sobrepreso" si está entre 25 y 29.9
+// "Obeso" si es igual o mayor a 30 
+
+// Ejemplo 
+// console.log( /imc(65,1.8))//normal
+// console.log( /imc(72,1.6))//sobrepeso
+// console.log( /imc(52,1.75))//bajo de peso
+// console.log( /imc(135,1.7))//obeso
+

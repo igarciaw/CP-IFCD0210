4896de83a7505aef63b7944cf3fb648f3600d3ea
Irene García <irenegwodak@gmail.com>
2025-03-04 14:20:02 +0100

    Add 24: conversor celsius farenheit

---
+++ 03JS/Ejercicios/ENTREGABLES/24-sustitutivo-convertir-de-celsius-a-farenheit/script.js
@@ -0,0 +1,5 @@
+// [SUSTITUTIVO] Convertir de Celsius a Farenheit
+// Escriba un programa en Javascript que dada una temperatura en Celsius debe mostrar la temperatura en Farenheit.
+
+// Para convertir una temperatura en grados Celsius se divide por 9/5 y se suma 32. 
+// Es decir - F = (C x 9/5) + 32
